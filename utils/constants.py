fe1162ca178aeee81f486a404a462cfb04e6a094
Beakal Lemeneh <44151344+bkmulusew@users.noreply.github.com>
2024-09-02 18:10:36 -0400

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -6,7 +6,7 @@ class ModelConfig:
     OUTPUT_CHUNK_LENGTH: int = 1
     N_EPOCHS: int = 3
     BATCH_SIZE: int = 1024
-    DATA_FILE_PATH: str = "ml_pairs_trading/sample_data.csv"
+    DATA_FILE_PATH: str = "ml_fx_trading/dataset/exchange_rate.csv"
     TRAIN_RATIO: float = 0.5
     WALLET_A: float = 10000.0
     WALLET_B: float = 10000.0
